5d909675951179b87175665d0957a01c084d76af
Matheus Abegg <mabegg@thoughtworks.com>
2014-04-16 17:21:39 -0300

    improve list router spec

--- database.rb
+++ database.rb
@@ -4,15 +4,15 @@ require 'sinatra/sequel'
 migration 'create the lists table' do
   database.create_table :lists do
     primary_key :id
-    text        :name, :null => false
-    timestamp   :create_at
+    text :name, :null => false
+    timestamp :create_at
   end
 end
 
 migration 'create the items table' do
   database.create_table :items do
     primary_key :id
-    text        :name, :null => false
+    text :name, :null => false
     foreign_key :list_id, :lists => :id
   end
 end
--- spec/routes/lists_spec.rb
+++ spec/routes/lists_spec.rb
@@ -2,18 +2,20 @@ require 'spec_helper'
 
 describe 'GET lists/:id' do
 
-   context 'when list exists' do
-      before :each do
-         List.stub(:[]).with('1').and_return(List.new)
-         get '/lists/1'
-      end
+  context 'when list exists' do
+    let(:the_list) { List.new }
+    let(:list_id) { '1' }
+    before :each do
+      List.stub(:[]).with('1').and_return(the_list)
+      get "/lists/#{list_id}"
+    end
 
-      it 'responds with success' do
-         expect(last_response.status).to be 200
-      end
+    it 'responds with success' do
+      expect(last_response.status).to be 200
+    end
 
-      it 'renders the list JSON' do
-         expect(last_response.body).to eq '{}'   
-      end
-   end
+    it 'renders the list JSON' do
+      expect(last_response.body).to eq '{}'
+    end
+  end
 end
