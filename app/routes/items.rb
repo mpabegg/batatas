f046e7abe2db7394a7467d34f6f1f5e1425d45b6
Matheus Abegg <mabegg@thoughtworks.com>
2014-06-30 22:24:36 -0300

    start implementing items route

--- app/routes/items.rb
+++ app/routes/items.rb
@@ -1,3 +1,6 @@
-get '/items/:id/?' do
-  json {}
+post '/lists/:list_id/items/?' do
+  list = List[params[:list_id]]
+  halt 404 unless list
+
+  status 201
 end
--- spec/routes/items_spec.rb
+++ spec/routes/items_spec.rb
@@ -8,6 +8,34 @@ describe Item do
   end
 
   describe 'POST /lists/:list_id/items' do
+    context 'when list does not exist' do
+      it 'responds with not found' do
+        post '/lists/blah/items/', {}, {'CONTENT_TYPE' => 'application/json'}
 
+        expect(last_response.status).to be 404
+      end
+
+      it 'responds with empty body' do
+        post '/lists/blah/items/', {}, {'CONTENT_TYPE' => 'application/json'}
+
+        expect(last_response.body).to eq ''
+      end
+    end
+
+    context 'when list exists' do
+      let(:list) { List.create(name: 'Cheesecake') }
+
+      context 'and body is empty' do
+        xit 'responds with unprocessable entity' do
+
+        end
+      end
+
+      it 'responds with no content' do
+        post "/lists/#{list.id}/items/", {}, {'CONTENT_TYPE' => 'application/json'}
+
+        expect(last_response.status).to be 201
+      end
+    end
   end
 end
