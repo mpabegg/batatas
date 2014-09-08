733fb0779a9070d1fc461c8ebfa7943c2441e09a
Matheus Abegg <mabegg@thoughtworks.com>
2014-09-08 20:31:18 -0300

    loggin request for non prod

--- server.rb
+++ server.rb
@@ -14,3 +14,26 @@ end
 before do
   content_type 'application/json'
 end
+
+
+# Copied form https://raw.githubusercontent.com/macedo/sinatra-contrib/master/lib/sinatra/request_logger.rb
+unless ENV['RACK_ENV'] == 'production'
+  set :logging, true
+  before do
+    request_query_string = if request.query_string.empty?
+                             request.query_string
+                           else
+                             "?#{request.query_string}"
+                           end
+    logger.info(
+        "Starting #{request.request_method} #{request.path}#{request_query_string} for #{request.ip}"
+    )
+  end
+
+  after do
+    if response.body.respond_to? :join
+      logger.info("Response: #{response.body.join("")}")
+    end
+    logger.info("Completed #{request.request_method} #{request.path} with status #{response.status}")
+  end
+end
